2051721b845c54b6a67dde705e24250c28db80ae
Prafful <46891804+iamsahu@users.noreply.github.com>
2021-08-23 16:03:59 +0530

    Update connector.js
    Added support for matic

--- src/helpers/connector.js
+++ src/helpers/connector.js
@@ -1,5 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 
 export const injected = new InjectedConnector({
-	supportedChainIds: [1337, 4, 42, 0, 5, 3],
+	supportedChainIds: [1337, 4, 42, 0, 5, 3,137],
 });
